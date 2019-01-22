3562b0c2aed12fdbe94d46014036ed56da661b99
Raz <razvansalaru@gmail.com>
2019-01-22 15:13:53 +0100

    prova connessione

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  //http: HTTP;
+  constructor(public navCtrl: NavController, private http: HTTP) {
 
   }
 
+  controlla(): void {
+    console.log("ok")
+    this.http.get('http://localhost:8080/APP_DIARIO/log/isalive', {}, {})
+      .then(data => {
+        console.log("va");
+      })
+      .catch(error => {
+        console.log("non va");
+      });
+    }
 }

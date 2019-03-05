9fb4415996923872ab2396d71b11e51aba3aa8cf
Raz <razvansalaru@gmail.com>
2019-03-05 12:40:09 +0100

    finish calendar with slides

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slide, Slides } from 'ionic-angular';
 
 /**
  * Generated class for the CalendarPage page.
@@ -15,105 +15,91 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CalendarPage {
 
+	@ViewChild("myslide") slides: Slides
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  months = []
+	monthsy = [8, 9, 10, 11, 0, 1, 2, 3, 4, 5]
+	
+	ionViewWillEnter(){
 
-  ionViewDidLoad() {
+		let today = new Date()
+		for(let i = 0; i < this.monthsy.length; i++){
+			if(today.getMonth() == this.monthsy[i]){
+				this.slides.slideTo(i, 0)
+			}
+		}
+	}
 
-    for(let i = new Date().getMonth(); i < 12; i++) {
-      this.months.push(i)
-    }
-    
-    for(let m of this.months) {
-      this.createCalendar(m)
-    }
-  }
+	createCalendar(month) {
+		this.calendar(new Date(new Date().getFullYear(), month))
+	}
 
-  
-createCalendar(month) {
-	//this.funziona(new Date(new Date().getFullYear(), month))
-}
 
-prova(){
-  console.log("ciao")
-}
-
-/*
  calendar(data){
-	let months = ["Gennario", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+		let months = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"]
+		let div = document.getElementById(data.getMonth())
+    let table = document.createElement("table")
+    table.id = "calendar"
+    let days = ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"]
   
-  let fuu = document.getElementById("fuu")
-    console.log(fuu)
-    let table = document.createElement("table");
-    table.id = "calendar";
-    let days = ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"];
-  
-   	table.innerHTML += `<caption id="cap" data-year="${data.getFullYear()}" data-month="${data.getMonth()}">${months[data.getMonth()]}</caption>`;
-    let tr = document.createElement("tr");
+   	table.innerHTML += `<caption id="cap" data-year="${data.getFullYear()}" data-month="${data.getMonth()}">${months[data.getMonth()]}</caption>`
+    let tr = document.createElement("tr")
     for(let i = 0; i < 7; i++){
-    	let el = document.createElement("td");
-        el.innerHTML = days[i];
-        tr.appendChild(el);
-        table.appendChild(tr);
+    		let el = document.createElement("td")
+        el.innerHTML = days[i]
+        tr.appendChild(el)
+        table.appendChild(tr)
     }
     	
-  	let gg = 1;
-    let finish = false;
+  	let g = 1
+    let finish = false
   	while(!finish){
-   		let ttr = document.createElement("tr");
+   		let ttr = document.createElement("tr")
       	for(let j = 1; j < 8; j++){
-        	let cc = new Date(data.getFullYear(), data.getMonth(), gg);
+        	let cc = new Date(data.getFullYear(), data.getMonth(), g)
         	if(j != 7) {
-        		if(cc.getDay() == j && gg <= this.giorni(cc.getFullYear(), cc.getMonth() + 1)){
-            		let ttd = document.createElement("td");
-            		ttd.innerHTML = (cc.getDate()).toString();
-    
-            		ttr.appendChild(ttd);
-            		if(gg != this.giorni(cc.getFullYear(), cc.getMonth() + 1)){
-                    	gg++;
-                    }
+        		if(cc.getDay() == j && g <= this.getDays(cc.getFullYear(), cc.getMonth() + 1)){
+            	let ttd = document.createElement("td")
+            	ttd.innerHTML = (cc.getDate()).toString()
+            	ttr.appendChild(ttd)
+            	if(g != this.getDays(cc.getFullYear(), cc.getMonth() + 1)){
+              	g++
+              }
        			}
-          		else{
-            		let ttd = document.createElement("td");
-            		ttd.innerHTML = "/";
-            		ttr.appendChild(ttd);
-          		}
+          	else{
+            	let ttd = document.createElement("td")
+            	ttd.innerHTML = "/"
+            	ttr.appendChild(ttd)
+          	}
         	}
         	else if( j == 7) {
-         		if(cc.getDay() == 0 && gg <= this.giorni(cc.getFullYear(), cc.getMonth() +1)) {
-            		let ttd = document.createElement("td");
-           	 		ttd.innerHTML = (cc.getDate()).toString();
-            		ttr.appendChild(ttd);
-            		if(gg != this.giorni(cc.getFullYear(), cc.getMonth() + 1)){
-                    	gg++;
-                    }
-          		}
-                else{
-            		var ttd = document.createElement("td");
-            		ttd.innerHTML = "/";
-            		ttr.appendChild(ttd);
-          		}
+         		if(cc.getDay() == 0 && g <= this.getDays(cc.getFullYear(), cc.getMonth() +1)) {
+            	let ttd = document.createElement("td")
+           	 	ttd.innerHTML = (cc.getDate()).toString()
+            	ttr.appendChild(ttd)
+            	if(g != this.getDays(cc.getFullYear(), cc.getMonth() + 1)){
+              	g++
+              }
+          	}
+            else{
+            	let ttd = document.createElement("td")
+            	ttd.innerHTML = "/"
+            	ttr.appendChild(ttd)
+          	}
         	}
-            if(gg >= this.giorni(cc.getFullYear(), cc.getMonth() + 1)) {
-             	finish = true;         
-            }
-            
-		}
-		
-    	table.appendChild(ttr);
+          if(g >= this.getDays(cc.getFullYear(), cc.getMonth() + 1)) {
+          	finish = true        
+          }
+        }
+				table.appendChild(ttr);
+			}
+			div.removeChild(div.firstChild)
+		div.appendChild(table)
 	}
-    fuu.appendChild(table);
 
-}
-*/
-
- giorni(anno, mese) {
-
-	return new Date(anno, mese, 0).getDate();
-}
-
-
-
+	getDays(anno, mese) {
+		return new Date(anno, mese, 0).getDate();
+	}	
 }

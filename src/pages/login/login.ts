cfe990a34e86b7cf98e77847c1233bea54d9d29b
mirko <mirko>
2019-03-06 11:56:01 +0100

    login css

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -6,9 +6,7 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'login.html',
-
+  selector: 'page-home', templateUrl: 'login.html',
 })
 
 export class LoginPage {
@@ -16,49 +14,49 @@ export class LoginPage {
   @ViewChild('user') user: HTMLInputElement
   @ViewChild('pass') pass: HTMLInputElement
 
-  data = {
-    "username": "",
-    "password": ""
-  }
+  data = { "username": "", "password": "" }
 
-  constructor(public navCtrl: NavController, public api: DiarioApiProvider) {
-  }
-
+  constructor(public navCtrl: NavController, public api: DiarioApiProvider) { }
 
   controlla(): void {
     this.data.username = this.user.value
     this.data.password = this.pass.value
-
     this.api.post('/login', this.data)
       .subscribe(data => {
         if(data != null) {
-          console.log(data)
-          //localStorage.setItem("jwt", data["jwt"])
-          if(data["username"]) {
-            localStorage.setItem("username", data["username"])
-            localStorage.setItem("privilege", data["privilege"])
-            if(localStorage.getItem("privilege") == "4")
-            this.navCtrl.setRoot(BackofficeMenuPage)
-            else
-            this.navCtrl.setRoot(HomePage)
+          if(data["status"] == "incorrect"){
+            console.log("else")
+            document.getElementById("error").style.display = "block";
+            document.getElementById("error").innerHTML = "Credenziali sbagliate"
           }
-        }
-        else {
-          console.log("else")
-        /*  document.getElementById("error").style.display = "block";
-          document.getElementById("error").innerHTML = "Credenziali sbagliate"*/
-        }
-      },
-      error => {
-        console.log("error")
-       /* document.getElementById("error").style.display = "block";
-        document.getElementById("error").innerHTML = "Credenziali sbagliate"*/
-      })
-  }
+          else if(data["status"] == "inexistent"){
+            console.log("else")
+            document.getElementById("error").style.display = "block";
+            document.getElementById("error").innerHTML = "Username inesistente"
+          }
+          else {
+            console.log(data)
 
-  ionViewDidLoad(){
-   if(localStorage.getItem('username') != null){
-     this.navCtrl.setRoot(HomePage)
-   }
-  }
-}
+            if(data["username"]) {
+              localStorage.setItem("username", data["username"])
+              localStorage.setItem("privilege", data["privilege"])
+              if(localStorage.getItem("privilege") == "4")
+               this.navCtrl.setRoot(BackofficeMenuPage)
+               else this.navCtrl.setRoot(HomePage)
+              }
+            }
+          }
+        },
+        error => {
+          console.log("error")
+          document.getElementById("error").style.display = "block";
+          document.getElementById("error").innerHTML = "Credenziali sbagliate"
+        })
+      }
+
+      ionViewDidLoad(){
+        if(localStorage.getItem('username') != null){
+          this.navCtrl.setRoot(HomePage)
+        }
+      }
+    }

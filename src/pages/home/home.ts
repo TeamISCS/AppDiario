b14ea003e2ebcd402503406debabd919797038da
Raz <razvansalaru@gmail.com>
2019-03-06 14:11:19 +0100

    fatto cose

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,7 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 import { UserPage } from '../user/user';
 import { SearchPage } from '../search/search';
+import { isNull } from 'util';
 
 
 @IonicPage()
@@ -20,6 +21,8 @@ export class HomePage {
   @ViewChild("myslide") slides: Slides
 
   ionViewWillEnter(){
+    document.getElementById("welcome").innerHTML =
+      `Benvenuto ${localStorage.getItem('name')} ${localStorage.getItem('surname')}`
 
 	  let today = new Date()
 		  for(let i = 0; i < this.monthsy.length; i++){
@@ -31,54 +34,53 @@ export class HomePage {
 
 
   ionViewDidLoad() {
-    if(localStorage.getItem("username") != null) {
-
-      if(localStorage.getItem("privilege") == "1") {
-        this.api.get(`/api/student/info/${localStorage.getItem("username")}`)
-        .subscribe(student => {
-          localStorage.setItem("name", student["name"]);
-          localStorage.setItem("surname", student["surname"])
-          localStorage.setItem("CF", student["CF"])
-          localStorage.setItem("birth_place", student["birth_place"])
-          localStorage.setItem("gender", student["gender"])
-          document.getElementById("welcome").innerHTML =
-              `Benvenuto ${localStorage.getItem('name')} ${localStorage.getItem('surname')}`
-        })
+    if(localStorage.getItem("logged") == "true") {
+      if(localStorage.getItem("name") == null){
+        if(localStorage.getItem("privilege") == "1") {
+          this.api.get(`/api/student/info/${localStorage.getItem("username")}`)
+          .subscribe(student => {
+            console.log(student)
+            localStorage.setItem("name", student["name"]);
+            localStorage.setItem("surname", student["surname"])
+            localStorage.setItem("CF", student["CF"])
+            localStorage.setItem("birth_place", student["birth_place"])
+            localStorage.setItem("gender", student["gender"])
+          
+          })
+        }
+        else if(localStorage.getItem("privilege") == "2") {
+          this.api.get(`/api/parent/info/${localStorage.getItem("username")}`)
+          .subscribe(parent => {
+            console.log(parent)
+            localStorage.setItem("name", parent["name"]);
+            localStorage.setItem("surname", parent["surname"])
+            localStorage.setItem("CF", parent["CF"])
+            localStorage.setItem("birth_place", parent["birth_place"])
+            localStorage.setItem("gender", parent["gender"])
+          })
+        }
+        else if(localStorage.getItem("privilege") == "3") {
+          this.api.get(`/api/teacher/info/${localStorage.getItem("username")}`)
+          .subscribe(teacher => {
+            console.log(teacher)
+            localStorage.setItem("name", teacher["name"]);
+            localStorage.setItem("surname", teacher["surname"])
+            localStorage.setItem("CF", teacher["CF"])
+            localStorage.setItem("birth_place", teacher["birth_place"])
+            localStorage.setItem("gender", teacher["gender"])
+          })
+        }
       }
-      else if(localStorage.getItem("privilege") == "2") {
-        this.api.get(`/api/parent/info/${localStorage.getItem("username")}`)
-        .subscribe(parent => {
-          localStorage.setItem("name", parent["name"]);
-          localStorage.setItem("surname", parent["surname"])
-          localStorage.setItem("CF", parent["CF"])
-          localStorage.setItem("birth_place", parent["birth_place"])
-          localStorage.setItem("gender", parent["gender"])
-          document.getElementById("welcome").innerHTML =
-              `Benvenuto ${localStorage.getItem('name')} ${localStorage.getItem('surname')}`
-        })
-      }
-      else if(localStorage.getItem("privilege") == "3") {
-        this.api.get(`/api/teacher/info/${localStorage.getItem("username")}`)
-        .subscribe(teacher => {
-          localStorage.setItem("name", teacher["name"]);
-          localStorage.setItem("surname", teacher["surname"])
-          localStorage.setItem("CF", teacher["CF"])
-          localStorage.setItem("birth_place", teacher["birth_place"])
-          localStorage.setItem("gender", teacher["gender"])
-          document.getElementById("welcome").innerHTML =
-              `Benvenuto ${localStorage.getItem('name')} ${localStorage.getItem('surname')}`
-        })
-      }
-
     }
     else {
-    this.navCtrl.setRoot(LoginPage)
+      this.navCtrl.setRoot(LoginPage)
     }
   }
 
 
   logout() {
     localStorage.removeItem("jwt")
+    localStorage.setItem("logged", "false")
     localStorage.removeItem("username")
     localStorage.removeItem("name")
     localStorage.removeItem("surname")
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,6 @@
 import { BackofficeMenuPage } from './../backoffice/backoffice-menu/backoffice-menu';
 import { HomePage } from './../home/home';
 import { DiarioApiProvider } from './../../providers/diario-api/diario-api';
-import { RegisterPage } from './../register/register';
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -22,41 +21,40 @@ export class LoginPage {
     this.data.username = this.user.value
     this.data.password = this.pass.value
     this.api.post('/login', this.data)
-      .subscribe(data => {
-        if(data != null) {
-          if(data["status"] == "incorrect"){
-            console.log("else")
-            document.getElementById("error").style.display = "block";
-            document.getElementById("error").innerHTML = "Credenziali sbagliate"
-          }
-          else if(data["status"] == "inexistent"){
-            console.log("else")
-            document.getElementById("error").style.display = "block";
-            document.getElementById("error").innerHTML = "Username inesistente"
+    .subscribe(data => {
+      if(data["status"] == "incorrect"){
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").innerHTML = "Credenziali sbagliate"
+      }
+      else if(data["status"] == "inexistent"){
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").innerHTML = "Username inesistente"
+      }
+      else {
+        console.log(data)
+        if(data["username"]) {
+          localStorage.setItem("logged", "true");
+          localStorage.setItem("username", data["username"])
+          localStorage.setItem("privilege", data["privilege"])
+          if(localStorage.getItem("privilege") == "4") {
+            this.navCtrl.setRoot(BackofficeMenuPage)
           }
           else {
-            console.log(data)
-
-            if(data["username"]) {
-              localStorage.setItem("username", data["username"])
-              localStorage.setItem("privilege", data["privilege"])
-              if(localStorage.getItem("privilege") == "4")
-               this.navCtrl.setRoot(BackofficeMenuPage)
-               else this.navCtrl.setRoot(HomePage)
-              }
-            }
+            this.navCtrl.setRoot(HomePage)
           }
-        },
-        error => {
-          console.log("error")
-          document.getElementById("error").style.display = "block";
-          document.getElementById("error").innerHTML = "Credenziali sbagliate"
-        })
-      }
-
-      ionViewDidLoad(){
-        if(localStorage.getItem('username') != null){
-          this.navCtrl.setRoot(HomePage)
         }
       }
+    },
+    error => {
+      console.log("error")
+      document.getElementById("error").style.display = "block";
+      document.getElementById("error").innerHTML = "Credenziali sbagliate"
+    })
+  }
+
+  ionViewDidLoad(){
+    if(localStorage.getItem('logged') == "true"){
+      this.navCtrl.setRoot(HomePage)
     }
+  }
+}
